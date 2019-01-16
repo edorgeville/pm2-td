158379f7ab8493cb96d47327e8c8bdcc6c0c3afe
Erwan d'Orgeville <info@erwandorgeville.com>
2019-01-16 13:29:30 -0500

    Update from @Craigson#6556

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,20 +1,13 @@
-module.exports = {
-  apps : [{
-    name: 'td',
-    script: 'pm2-td.toe',
+const touchDesigner = {
+  name: 'TouchDesigner',
+  interpreter:'C:/Program Files/Derivative/TouchDesigner099/bin/TouchDesigner099.exe',
+  script: 'pm2-td.toe',
+  env: {
+    COMMON_VARIABLE: 'true'
+  },
+  env_production: {
+    NODE_ENV: 'production'
+  }
+}
 
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    //args: 'one two',
-    //instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    interpreter: 'C:/Program Files/Derivative/TouchDesigner099/bin/TouchDesigner099.exe'
-    // env: {
-    //   NODE_ENV: 'development'
-    // },
-    // env_production: {
-    //   NODE_ENV: 'production'
-    // }
-  }]
-};
+exports.apps = [touchDesigner]
